7273c9338f3cb59de0ae02431c6726419df80df8
Dino <32899228+Hollow-Serenity@users.noreply.github.com>
2022-11-23 18:49:41 +0100

    Update ExpenseForm.js
    The whole point of making ExpenseForm better, is passing a pointer as a function.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -26,7 +26,7 @@ const ExpenseForm = () => {
       date: new Date(enteredDate),
     };
 
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
